a0c4b8d4aead8e6ea3bbf70ab53a271a065e187f
David Hartmann <dhartmann@uni-mainz.de>
2023-03-16 10:59:41 +0100

    implemented basic adnw layout

--- keyboards/backontrack44/keymaps/default/keymap.c
+++ keyboards/backontrack44/keymaps/default/keymap.c
@@ -2,6 +2,25 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_german.h"
+
+
+enum layers {
+    _ADNW,
+    _PROG,
+    _NAVI,
+    _SPECIAL,
+};
+
+
+#define M_PROG MO(_PROG)
+#define M_NAVI MO(_NAVI)
+
+// define M_SPECIAL = M_NAVI + M_PROG
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _NAVI, _PROG, _SPECIAL);
+}
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -24,13 +43,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       *
       */
-    [0] = LAYOUT_split_3x6_4_2(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    [_ADNW] = LAYOUT_split_3x6_4_2(
+        KC_TAB,      KC_K,       KC_U,       DE_UDIA,    KC_DOT,     DE_ADIA,                KC_V,       KC_G,       KC_C,    KC_L,    KC_J,    KC_F,
+        M_PROG,      KC_H,       KC_I,       KC_E,       KC_A,       KC_O,                   KC_D,       KC_T,       KC_R,    KC_N,    KC_S,    M_PROG,
+        KC_LSFT,     KC_X,       KC_Y,       DE_ODIA,    KC_MINS,    KC_Q,                   KC_B,       KC_P,       KC_W,    KC_M,    KC_Z,    KC_RSFT,
+                                 KC_LCTL,    KC_LGUI,    M_NAVI,     KC_SPC,                 KC_ENT,     M_NAVI,     M_PROG,  KC_LALT,
+                                                         KC_1,       KC_2,                   KC_3,       KC_4
+    ),
 
-                          KC_1,    KC_1,    KC_1,    KC_1,             KC_1,    KC_1,    KC_1,    KC_1, 
+    [_PROG] = LAYOUT_split_3x6_4_2(
+        _______,     _______, KC_UNDERSCORE, KC_LBRC,    KC_RBRC,    KC_CIRC,                KC_EXLM,    KC_LT,   KC_GT,   KC_EQL,     KC_PLUS, KC_MINUS,
+        _______,     KC_BSLS, KC_SLASH,      KC_LCBR,    KC_RCBR,    KC_ASTR,                KC_QUES,    KC_LPRN, KC_RPRN, KC_MINUS,   KC_COLON,_______,
+        _______,     KC_HASH, KC_DOLLAR,     KC_PIPE,    KC_TILDE,   KC_GRAVE,               KC_PLUS,    KC_PERC, KC_DQUO, KC_QUOTE,   KC_SCLN, _______,
+                                 _______,    _______,    _______,    _______,                _______,    _______,    _______, _______,
+                                                         _______,    _______,                _______,    _______
+    ),
 
-                                            KC_1,    KC_1,             KC_1,    KC_1
+    [_NAVI] = LAYOUT_split_3x6_4_2(
+        _______,     KC_PAGE_UP, KC_BSPC,    KC_UP,      KC_DEL,     KC_PAGE_DOWN,           _______,       KC_7,    KC_8,    KC_9,    KC_PLUS, KC_MINUS,
+        _______,     KC_HOME,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_END,                 _______,       KC_4,    KC_5,    KC_6,    KC_COMMA,KC_AT,
+        _______,     KC_ESC,     KC_TAB,     KC_INSERT,  KC_RETURN,  KC_UNDO,                _______,       KC_1,    KC_2,    KC_3,    KC_SCLN, _______,
+                                 _______,    _______,    _______,    _______,                _______,    _______,    _______, _______,
+                                                         _______,    _______,                _______,    _______
+    ),
+
+    [_SPECIAL] = LAYOUT_split_3x6_4_2(
+        KC__MUTE,    KC_MPLY,    KC_MPRV,    KC_MNXT,    KC__VOLDOWN,KC__VOLUP,              _______,    _______,    _______, _______,  _______, _______,
+        _______,     _______,    _______,    _______,    _______,    _______,                _______,    _______,    _______, _______,  _______, _______,
+        _______,     _______,    _______,    _______,    _______,    _______,                _______,    _______,    _______, _______,  _______, _______,
+                                 _______,    _______,    _______,    _______,                _______,    _______,    _______, _______,
+                                                         _______,    _______,                _______,    _______
     )
+
 };
+
+
