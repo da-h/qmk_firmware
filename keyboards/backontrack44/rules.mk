de5893b3876be289e686111f8929e94797ac02fa
David Hartmann <dhartmann@uni-mainz.de>
2023-04-06 09:01:12 +0200

    pointing device: added

--- keyboards/backontrack44/rules.mk
+++ keyboards/backontrack44/rules.mk
@@ -2,14 +2,21 @@
 
 RGBLIGHT_ENABLE = yes
 
-DYNAMIC_MACRO_ENABLE = yes
+# DYNAMIC_MACRO_ENABLE = yes
 
-CAPS_WORD_ENABLE = yes
+# CAPS_WORD_ENABLE = yes
 
 KEY_OVERRIDE_ENABLE = yes
 
-LEADER_ENABLE = yes
+# LEADER_ENABLE = yes
 
-UNICODE_ENABLE = yes
+# UNICODE_ENABLE = yes
+
+# trackpad
+LTO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
+# MOUSEKEY_ENABLE = yes       # Mouse keys
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+
 # OLED_ENABLE = yes
 # OLED_DRIVER = SSD1306    # OLED display
